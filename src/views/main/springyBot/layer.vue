4dbc95c5939e259f67d7c562e4fcea7a9f94d430
irlmpbfhvf2009 <hyprocrite1631@gmail.com>
2023-03-22 19:03:25 +0800

    init

--- src/views/main/springyBot/layer.vue
+++ src/views/main/springyBot/layer.vue
@@ -69,8 +69,8 @@ export default defineComponent({
     const ruleForm= ref(null)
     const layerDom = ref(null)
     let form = ref({
-      token: '',
-      username:'',
+      token: '5855785269:AAH9bvPpYudd2wSAvMnBTiKakCeoB92_Z_8',
+      username:'CCP_1121_BOT',
       state:false,
       config:{
         inviteFriendsSet:false,
