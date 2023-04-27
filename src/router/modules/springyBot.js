f9ade5dfac445655fd61cdfc0703d9fd1c78abb9
irlmpbfhvf2009 <hyprocrite1631@gmail.com>
2023-04-27 11:53:47 +0800

    fix

--- src/router/modules/springyBot.js
+++ src/router/modules/springyBot.js
@@ -21,7 +21,7 @@ const route = [
       {
         path: 'jobManagement',
         component: createNameComponent(() => import('@/views/main/springyBot/jobManagement/index.vue')),
-        meta: { title: '招聘和求职信息管理(尚未开发)', cache: true, roles: ['admin'] }
+        meta: { title: '招聘和求职信息管理', cache: true, roles: ['admin'] }
       },
       // {
       //   path: 'robotGroupManagement2',
